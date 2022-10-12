99dfaecd5d72cb35d412e90fe67d9d8bc774f1f4
Vasilisa Douglas <vasilisa.douglas@gmail.com>
2022-10-11 19:56:42 -0500

    commit 2

--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -3,6 +3,9 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+#include <Adafruit_LSM6DSOX.h>
+#include <Adafruit_LIS3MDL.h>
+
 
 #define LED_PIN 25
 
@@ -13,6 +16,8 @@
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BMP3XX bmp;
+Adafruit_LSM6DSOX lsm6ds;
+Adafruit_LIS3MDL lis3mdl;
 
 void setup() {
   // Initialize LED_PIN as an output
@@ -42,6 +47,79 @@ void setup() {
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
+  bool lsm6ds_success;
+  bool lis3mdl_success;
+  lsm6ds_success = lsm6ds.begin_I2C();
+  lis3mdl_success = lis3mdl.begin_I2C();
+
+  if (!lsm6ds_success){
+    Serial.println("Failed to find LSM6DS chip");
+  }
+  if (!lis3mdl_success){
+    Serial.println("Failed to find LIS3MDL chip");
+  }
+  if (!(lsm6ds_success && lis3mdl_success)) {
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DS and LIS3MDL Found!");
+
+  // lsm6ds.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (lsm6ds.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+  // lsm6ds.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (lsm6ds.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
 }
 
 void loop() {
@@ -65,6 +143,8 @@ void loop() {
     return;
   }
 
+  sensors_event_t accel;
+
   Serial.print(bmp.temperature);    //temp
   Serial.print(", \t");
 
@@ -73,6 +153,15 @@ void loop() {
 
   Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));   //approx altitude
   Serial.print(", \t");
+
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x, 4);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y, 4);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z, 4);
+  Serial.println(" \tm/s^2 ");
+  
 
   Serial.println();
   delay(2000);
